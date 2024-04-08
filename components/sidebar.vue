483ddbc591a22540ffc0725b6680c60c74ae6692
kbcode85 <kacper.baranowski00@gmail.com>
2024-04-09 00:37:18 +0200

    poprawki sidebar

--- components/sidebar.vue
+++ components/sidebar.vue
@@ -1,7 +1,7 @@
 <template>
-	<div class="d-flex flex-column flex-shrink-0 p-3 text-bg-dark" style="width: 280px; height: 100vh">
+	<div class="d-flex flex-column flex-shrink-0 p-3 text-bg-dark vh-100" style="width: 280px">
 		<a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
-			<img src="/assets/img/logo.png" alt="Favicon" class="bi pe-none me-2" width="40" height="32" />
+			<img src="/assets/img/logo.png" alt="Favicon" class="bi pe-none me-2" width="40" height="40" />
 			<span class="fs-4">HMSuite</span>
 		</a>
 		<hr />
