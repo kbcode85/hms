9ff27e5f1c2d3713eed935daf4db30453fa08f1d
Kacper Baranowski <dev@kbdev.pl>
2024-04-17 00:10:14 +0200

    Add guest model and prepare for data validation

---
+++ server/models/guest.ts
@@ -0,0 +1,36 @@
+type Branded<T, B extends string> = T & { __brand: B };
+
+type Name = Branded<string, "Name">;
+type Surname = Branded<string, "Surname">;
+type Phone = Branded<string, "Phone">;
+type Email = Branded<string, "Email">;
+type Address = Branded<string, "Address">;
+type City = Branded<string, "City">;
+type Country = Branded<string, "Country">;
+type ZipCode = Branded<string, "ZipCode">;
+type Description = Branded<string, "Description">;
+
+interface Guest {
+  name: Name;
+  surname: Surname;
+  phone: Phone;
+  email: Email;
+  address: Address;
+  city: City;
+  country: Country;
+  zip_code: ZipCode;
+  description: Description;
+}
+
+export {
+  Guest,
+  Name,
+  Surname,
+  Phone,
+  Email,
+  Address,
+  City,
+  Country,
+  ZipCode,
+  Description,
+};
